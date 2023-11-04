0e4803debfd2a8c4e75bdd4ea92effac67235e2b
Loyce <chenty02@qq.com>
2023-11-04 23:01:38 +0800

    Introduce new nodes for Gcd

--- src/main/scala/Gcd/Main.scala
+++ src/main/scala/Gcd/Main.scala
@@ -4,9 +4,8 @@ import chipsalliance.rocketchip.config.Parameters
 import chisel3._
 import chisel3.internal.sourceinfo.SourceInfo
 import chisel3.stage.ChiselStage
-import freechips.rocketchip.diplomacy.{LazyModule, LazyModuleImp, NexusNode, RenderedEdge, SimpleNodeImp, SinkNode, SourceNode, ValName}
-
-import scala.util.Random
+import chisel3.util.random.FibonacciLFSR
+import freechips.rocketchip.diplomacy._
 
 
 class ParamBundle(val w: Int) extends Bundle {
@@ -22,26 +21,56 @@ case class DownwardParam(width: Int)
 case class EdgeParam(width: Int)
 
 object GcdNodeImp extends SimpleNodeImp[DownwardParam, UpwardParam, EdgeParam, ParamBundle] {
-  def edge(down: DownwardParam, up: UpwardParam, p: Parameters, sourceInfo: SourceInfo): EdgeParam = {
-    EdgeParam(math.max(down.width, up.width))
+  override def edge(pd: DownwardParam, pu: UpwardParam, p: Parameters, sourceInfo: SourceInfo): EdgeParam = {
+    EdgeParam(math.max(pd.width, pu.width))
   }
 
-  def bundle(edge: EdgeParam) = new ParamBundle(edge.width)
+  override def bundle(e: EdgeParam) = new ParamBundle(e.width)
 
-  def render(edge: EdgeParam) = RenderedEdge("blue", s"width = ${edge.width}")
+  override def render(e: EdgeParam) = RenderedEdge("blue", s"width = ${e.width}")
+}
+
+object GcdDriverNodeImp extends SimpleNodeImp[DownwardParam, UpwardParam, EdgeParam, UInt] {
+  override def edge(pd: DownwardParam, pu: UpwardParam, p: Parameters, sourceInfo: SourceInfo): EdgeParam = {
+    EdgeParam(math.max(pd.width, pu.width))
+  }
+
+  override def bundle(e: EdgeParam): UInt = UInt(e.width.W)
+
+  override def render(e: EdgeParam): RenderedEdge = RenderedEdge("red", s"width = ${e.width}")
 }
 
 
 class GcdDriverNode(widths: Seq[DownwardParam])(implicit valName: ValName)
-  extends SourceNode(GcdNodeImp)(widths)
+  extends SourceNode(GcdDriverNodeImp)(widths)
 
-class GcdTestNode(width: UpwardParam)(implicit valName: ValName)
+class GcdTestInputNode(width: UpwardParam)(implicit valName: ValName)
+  extends SinkNode(GcdDriverNodeImp)(Seq(width))
+
+class GcdTestResultNode(width: UpwardParam)(implicit valName: ValName)
   extends SinkNode(GcdNodeImp)(Seq(width))
 
 class GcdNode(dFn: Seq[DownwardParam] => DownwardParam,
-              uFn: Seq[UpwardParam] => UpwardParam)(implicit valName: ValName)
-  extends NexusNode(GcdNodeImp)(dFn, uFn)
+              uFn: Seq[UpwardParam] => UpwardParam)
+             (implicit valName: ValName)
+  extends MixedNexusNode(GcdDriverNodeImp, GcdNodeImp)(dFn, uFn)
 
+class GcdDriver(width: Int, numOutputs: Int)(implicit p: Parameters) extends LazyModule {
+  val node = new GcdDriverNode(Seq.fill(numOutputs)(DownwardParam(width)))
+
+  lazy val module = new LazyModuleImp(this) {
+    val w = node.edges.out.head.width
+    val io = IO(new Bundle() {
+      val inputs = Input(UInt(w.W))
+    })
+    node.out.foreach {
+      case (num, _) =>
+        num := FibonacciLFSR.maxPeriod(w)
+    }
+  }
+
+  override lazy val desiredName = "GcdDriver"
+}
 
 class Gcd(implicit p: Parameters) extends LazyModule {
   val node = new GcdNode({
@@ -54,20 +83,19 @@ class Gcd(implicit p: Parameters) extends LazyModule {
       ups.head
   })
   lazy val module: LazyModuleImp = new LazyModuleImp(this) {
-    require(node.in.size >= 2)
-
     val size = node.in.size
-    val width = node.in.head._2.width
-    val start = node.in.head._1.start
+    require(size >= 2)
+    val width = node.edges.out.head.width
+    val start = RegInit(true.B)
     val numbers = RegInit(VecInit(Seq.fill(size)(0.U(width.W))))
     val busy = RegInit(false.B)
     val load = Wire(Bool())
-    val done = Wire(Bool())
+    val done = WireDefault(false.B)
     val minNum = numbers.reduce((x, y) => Mux(x > y, x, y))
 
     load := start && !busy
     when(!busy) {
-      when(start) {
+      when(start && !load) {
         busy := true.B
       }
     }.otherwise {
@@ -76,11 +104,8 @@ class Gcd(implicit p: Parameters) extends LazyModule {
       }
     }
     done := busy && numbers.map(_ === minNum).reduce(_ && _)
-
     when(load) {
-      for (i <- numbers.indices) {
-        numbers(i) := node.in(i)._1.number
-      }
+      numbers.zip(node.in.map(_._1)).foreach { case (num, input) => num := input }
     }.elsewhen(busy && !done) {
       for (i <- numbers.indices) {
         when(numbers(i) % minNum === 0.U) {
@@ -98,33 +123,17 @@ class Gcd(implicit p: Parameters) extends LazyModule {
   override lazy val desiredName = "Gcd"
 }
 
-class GcdDriver(width: Int, numOutputs: Int)(implicit p: Parameters) extends LazyModule {
-  val node = new GcdDriverNode(Seq.fill(numOutputs)(DownwardParam(width)))
-
-  lazy val module = new LazyModuleImp(this) {
-    val widths = node.edges.out.map(_.width)
-    val w = widths.head
-    node.out.foreach {
-      case (num, _) =>
-        num.start := true.B
-        num.number := (Random.nextInt() % (1 << w - 1) + 1).asUInt(w.W)
-        num.done := false.B
-    }
-  }
-
-  override lazy val desiredName = "GcdDriver"
-}
-
 class GcdTest(width: Int, numbers: Int)(implicit p: Parameters) extends LazyModule {
-  val nodeSeq = Seq.fill(numbers)(new GcdTestNode(UpwardParam(width)))
-  val nodeGcd = new GcdTestNode(UpwardParam(width))
+  val nodeInput = Seq.fill(numbers)(new GcdTestInputNode(UpwardParam(width)))
+  val nodeResult = new GcdTestResultNode(UpwardParam(width))
 
   lazy val module = new LazyModuleImp(this) {
     val io = IO(new Bundle {
       val error = Output(Bool())
     })
-    io.error := nodeGcd.in.head._1.done &&
-      nodeSeq.map(_.in.head._1.number % nodeGcd.in.head._1.number =/= 0.U).reduce(_ || _)
+    io.error :=
+      nodeResult.in.head._1.done &&
+        nodeInput.map(_.in.head._1 % nodeResult.in.head._1.number =/= 0.U).reduce(_ || _)
   }
 
   override lazy val desiredName = "GcdTest"
@@ -132,7 +141,7 @@ class GcdTest(width: Int, numbers: Int)(implicit p: Parameters) extends LazyModu
 
 class GcdTestHarness()(implicit p: Parameters) extends LazyModule {
   val numbers_size = 10
-  val gcd = LazyModule(new Gcd)
+  val gcd = LazyModule(new Gcd())
   val drivers = Seq.fill(numbers_size) {
     LazyModule(new GcdDriver(8, 2))
   }
@@ -140,10 +149,10 @@ class GcdTestHarness()(implicit p: Parameters) extends LazyModule {
   drivers.foreach {
     driver => gcd.node := driver.node
   }
-  drivers.zip(test.nodeSeq).foreach {
+  drivers.zip(test.nodeInput).foreach {
     case (driver, testNode) => testNode := driver.node
   }
-  test.nodeGcd := gcd.node
+  test.nodeResult := gcd.node
 
   lazy val module = new LazyModuleImp(this) {
     when(test.module.io.error) {
@@ -159,5 +168,5 @@ object Main extends App {
   println("DiplomacyGcd")
   (new ChiselStage).emitVerilog(
     LazyModule(new GcdTestHarness()(Parameters.empty)).module,
-    Array("--target-dir", "generated"))
+    Array("--target-dir", "generated/Gcd"))
 }
